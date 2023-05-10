8a01a552928dce2dd68f19bb397156b8c1142db8
Paweł hamryszak <phamryszak91@gmail.com>
2023-05-10 14:46:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Categories from "./Categories";
+import menu from "./data";
+import Menu from "./Menu";
+import Title from "./Title";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
+export default App;
---
+++ src/Title.jsx
@@ -0,0 +1,10 @@
+const Title = ({ text }) => {
+  return (
+    <div className="title">
+      <h2>{text || "Default title"}</h2>
+      <div className="title-underline"></div>
+    </div>
+  );
+};
+
+export default Title;
